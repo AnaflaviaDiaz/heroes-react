23050db7e3f30fe26d7c1626d660fefbd74f61b5
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-04-28 17:44:22 +0200

    feat: load HeroesApp in the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HeroesApp } from "./HeroesApp.tsx";
 import "./index.css";
-import { Button } from "./components/ui/button.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hola Mundo</h1>
-    <Button>Hola Mundo</Button>
+    <HeroesApp />
   </StrictMode>,
 );
