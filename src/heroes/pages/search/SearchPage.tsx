018c9468fabf9af23867354c50af332ffb67df39
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-04-28 17:37:26 +0200

    feat: add SearchPage

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,28 @@
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControl } from "@/heroes/ui/SearchControl";
+
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
-  )
-}
+    <>
+      {/* Header */}
+      <CustomJumbotron
+        title="Search Heroes"
+        subtitle="Find your favorite superheroes and villains"
+      />
+
+      {/* Breadcrumbs */}
+      <CustomBreadcrumbs
+        currentPage="Search"
+        // breadcrumbs={[{ label: "Home1", to: "/" }]}
+      />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+
+      {/* SearchControl */}
+      <SearchControl />
+    </>
+  );
+};
