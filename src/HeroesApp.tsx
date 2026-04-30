c2ef2d82ee5a9fca5f0b3fd58058ab2987884c55
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-04-30 17:42:35 +0200

    feat: init queryClient

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,11 +1,19 @@
 import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from "./router/app.router";
+
+const queryClient = new QueryClient();
 
 export const HeroesApp = () => {
   return (
     <>
-      {/* RouterProvider es para gestionar las rutas de la aplicación */}
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        {/* RouterProvider es para gestionar las rutas de la aplicación */}
+        <RouterProvider router={router} />
+
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </>
   );
 };
